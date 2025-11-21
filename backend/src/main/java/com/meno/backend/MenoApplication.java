bd2b912c19a35a425c1639f55b77a5569ac69a0e
max3796 <gps2005.singh@gmail.com>
2025-11-21 16:19:38 +0530

    project ini

---
+++ backend/src/main/java/com/meno/backend/MenoApplication.java
@@ -0,0 +1,13 @@
+package com.meno.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/meno/backend/MenoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meno.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
